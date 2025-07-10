6e413410e5eea4d1ebc0f2a4ecb06c3eda6dbca7
piyush mevada <piyushmevada1606@gmail.com>
2025-07-10 15:56:58 +0530

    day5 work is done

---
+++ day-5/Mission/Mission.Entities/Migrations/20250709062726_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EmailAddress", "FirstName", "LastName", "Password" },
+                values: new object[] { "piyush@gmail.com", "Piyush", "Mevada", "1617" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EmailAddress", "FirstName", "LastName", "Password" },
+                values: new object[] { "admin@tatvasoft.com", "Admin", "Tatva", "admin" });
+        }
+    }
+}
---
+++ day-5/Mission/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Models;
+
+namespace Mission.Entities
+{
+    public class MissionDbContext(DbContextOptions<MissionDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Piyush",
+                LastName = "Mevada",
+                EmailAddress = "piyush@gmail.com",
+                Password = "1617",
+                PhoneNumber = "01234567890",
+                UserType = "admin",
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
